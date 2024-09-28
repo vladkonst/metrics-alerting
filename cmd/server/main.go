f2fc564be7406313182574a3e6764b0a67caefef
vlad <vladkonst98@gmail.com>
2024-09-29 02:08:28 +0300

    first commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,72 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type MemStorage struct {
+	gauges   map[string]float64
+	counters map[string]int64
+}
+
+var memStorage = MemStorage{map[string]float64{}, map[string]int64{}}
+
+func updateGauge(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
+	}
+
+	pathValues := strings.Split(strings.Trim(r.URL.Path, "/"), "/gauge/")[1]
+	metricData := strings.Split(pathValues, "/")
+
+	if len(metricData) != 2 {
+		http.Error(w, "Metric not found.", http.StatusNotFound)
+		return
+	}
+
+	v, err := strconv.ParseFloat(metricData[1], 64)
+
+	if err != nil {
+		http.Error(w, "Bad request.", http.StatusBadRequest)
+		return
+	}
+
+	memStorage.gauges[metricData[0]] = v
+}
+
+func updateCounter(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+
+	pathValues := strings.Split(strings.Trim(r.URL.Path, "/"), "/counter/")[1]
+	metricData := strings.Split(pathValues, "/")
+
+	if len(metricData) != 2 {
+		http.Error(w, "Metric not found.", http.StatusNotFound)
+		return
+	}
+
+	v, err := strconv.ParseInt(metricData[1], 10, 64)
+
+	if err != nil {
+		http.Error(w, "Bad request.", http.StatusBadRequest)
+		return
+	}
+
+	memStorage.counters[metricData[0]] += v
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/update/gauge/", updateGauge)
+
+	mux.HandleFunc("/update/counter/", updateCounter)
+
+	mux.HandleFunc("/update/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { http.Error(w, "Bad request.", http.StatusBadRequest) }))
+
+	http.ListenAndServe("localhost:8080", mux)
+}
