bc313ba9979493ff8373f375caa14300e845dff3
vlad <vladkonst98@gmail.com>
2024-11-06 17:39:54 +0300

    fix: added Accept header check for compress middleware

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -44,6 +44,31 @@ func GzipMiddleware(h http.Handler) http.Handler {
 			}
 		default:
 		}
+		switch r.Header.Get("Accept") {
+		case "application/json", "text/html":
+			acceptEncoding := r.Header.Get("Accept-Encoding")
+			supportsGzip := strings.Contains(acceptEncoding, "gzip")
+			if supportsGzip {
+				cw := newCompressWriter(w)
+				ow = cw
+				defer cw.Close()
+				cw.Header().Add("Content-Encoding", "gzip")
+			}
+
+			contentEncoding := r.Header.Get("Content-Encoding")
+			sendsGzip := strings.Contains(contentEncoding, "gzip")
+			if sendsGzip {
+				cr, err := newCompressReader(r.Body)
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				r.Body = cr
+				defer cr.Close()
+			}
+		default:
+		}
 		h.ServeHTTP(ow, r)
 	})
 }
