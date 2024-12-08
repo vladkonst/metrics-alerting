b5f0fa432192d2c7701e4fc1c15d0390d7df523c
vlad <vladkonst98@gmail.com>
2024-12-08 18:28:46 +0300

    fix: changed config hash key default value

--- internal/configs/configs.go
+++ internal/configs/configs.go
@@ -22,7 +22,7 @@ func GetClientConfig() *ClientCfg {
 	intervalCfg := &ClientIntervalsCfg{ReportInterval: 10, PollInterval: 2}
 	flag.IntVar(&intervalCfg.ReportInterval, "r", intervalCfg.ReportInterval, "report interval to send metrics")
 	flag.IntVar(&intervalCfg.PollInterval, "p", intervalCfg.PollInterval, "poll interval to update metrics")
-	flag.StringVar(&intervalCfg.HashKey, "k", "dfshdfghjgfjksfghj46346734", "hash key")
+	flag.StringVar(&intervalCfg.HashKey, "k", "", "hash key")
 	addr := &NetAddressCfg{Host: "localhost", Port: 8080}
 	flag.Var(addr, "a", "Server net address host:port")
 	flag.Parse()
@@ -44,7 +44,7 @@ func GetServerConfig() *ServerCfg {
 	flag.IntVar(&intervalCfg.StoreInterval, "i", intervalCfg.StoreInterval, "store interval to load metrics to the file")
 	flag.StringVar(&intervalCfg.FileStoragePath, "f", intervalCfg.FileStoragePath, "file with stored metrics")
 	flag.StringVar(&intervalCfg.DatabaseDSN, "d", "", "database connection string")
-	flag.StringVar(&intervalCfg.HashKey, "k", "dfshdfghjgfjksfghj46346734", "hash key")
+	flag.StringVar(&intervalCfg.HashKey, "k", "", "hash key")
 	flag.BoolVar(&intervalCfg.Restore, "r", intervalCfg.Restore, "allow metrics load from file on server start")
 	flag.Parse()
 	if err := env.Parse(intervalCfg); err != nil {
